dc9fd29194206c8581699e98e35573da85a83e53
seiya2130 <satohseiya0312@gmail.com>
2020-01-19 14:32:42 +0900

    [update]タスクにバリデーションを追加
    タスク作成、更新にバリデーションの処理を追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,9 @@
 class Task < ApplicationRecord
   belongs_to :task_list
+  validates :name,  presence: true, length: { maximum: 20 }
+  validate  :date_not_before_today
+
+  def date_not_before_today
+    errors.add(:dead_line, "は今日日付より後の日付を選択してください") if dead_line < Date.today
+  end
 end
