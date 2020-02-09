eddf9d16adb4a8def0c9cf9c4dba524a961e1528
seiya2130 <satohseiya0312@gmail.com>
2020-02-09 18:40:43 +0900

    [update]パスワードダイエジェスト追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_06_233018) do
+ActiveRecord::Schema.define(version: 2020_02_09_093859) do
 
   create_table "task_lists", force: :cascade do |t|
     t.string "name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2020_02_06_233018) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
