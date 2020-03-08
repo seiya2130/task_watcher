175af68504478146d86f42ec2c6cd10f34a025f9
seiya2130 <satohseiya0312@gmail.com>
2020-03-08 16:38:13 +0900

    [fix]ログインのルーティングを修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in(user)
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      log_in(@user)
       flash[:notice] = "ログインしました"
-      redirect_to("/users/#{user.id}") 
+      redirect_to controller: 'users', action: 'show', id: @user
     else
       flash[:danger] = "メールアドレスまたはパスワードが誤っています"
-      render("/sessions/new")
+      render 'new'
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,23 @@
 Rails.application.routes.draw do
+  #セッション
   post '/guest_login', to:'sessions#guest'
   get '/login', to:'sessions#new'
   post '/login', to:'sessions#create'
   delete '/logout', to:'sessions#destroy'
   
+  #ユーザー
   resources :users, only: [:show, :edit, :update]
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
   
+  #タスクリスト・タスク
   resources :task_lists do
     resources :tasks, except: [:index, :show], shallow: true
   end
   get '/tasks/progress', to:'tasks#progress'
 
+  #トップ
   get 'static_pages/top'  
+
   root to: 'static_pages#top'
 end
