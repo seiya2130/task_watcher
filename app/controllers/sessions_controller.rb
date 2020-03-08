a90d4b033d1c43f68ec33b1785c9e35bc546a68e
seiya2130 <satohseiya0312@gmail.com>
2020-03-08 16:46:18 +0900

    [fix]#63 ゲストログインのルーティング修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(email: params[:session][:email].downcase)
-    if @user && @user.authenticate(params[:session][:password])
-      log_in(@user)
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in(user)
       flash[:notice] = "ログインしました"
-      redirect_to controller: 'users', action: 'show', id: @user
+      redirect_to controller: 'users', action: 'show', id: user
     else
       flash[:danger] = "メールアドレスまたはパスワードが誤っています"
       render 'new'
@@ -26,9 +26,9 @@ class SessionsController < ApplicationController
       user = User.new(name:"ゲスト",email:"guest@guest.com",password:"cde34rfv")
       user.save
     end
-    log_in(user)
-    flash[:notice] = "ゲストユーザーでログインしました"
-    redirect_to("/users/#{user.id}") 
+      log_in(user)
+      flash[:notice] = "ゲストユーザーでログインしました"
+      redirect_to user
   end
 
 end
