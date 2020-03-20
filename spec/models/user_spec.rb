cee76db23d4347f87d4460d6e888ceb30f120d5a
seiya2130 <satohseiya0312@gmail.com>
2020-03-20 21:32:06 +0900

    [fix]#14 モデルテスト修正
    ・ファイル名にspecを追記
    ・userモデル　共通化処理追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,95 +1,136 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-    describe 'name' do     
-      it '空白' do 
-        user = build(:user, name: nil)
+  describe 'name' do   
+    let(:user) { build(:user, name: name) }  
+    context '空白' do
+      let(:name){ nil }
+      it 'is invalid' do 
         expect(user).to be_invalid
         expect(user.errors.messages[:name]).to include("を入力してください")
       end
-      it '50文字以上' do 
-        user = build(:user, name: "a" * 51)
+    end
+    context '51文字以上' do 
+      let(:name){ "a" * 51 }
+      it 'is invalid' do 
         expect(user).to be_invalid
         expect(user.errors.messages[:name]).to include("は50文字以内で入力してください")
       end
     end
-    describe 'email' do        
-      it '空白' do 
-        user = build(:user, email: nil)
+  end
+  describe 'email' do
+    let(:user) { build(:user, email: email) }  
+    context '空白' do
+      let(:email){ nil }
+      it 'is invalid' do 
         expect(user).to be_invalid
         expect(user.errors.messages[:email]).to include("を入力してください")
       end
-      it '255文字以上' do 
-        user = build(:user, email: "a" * 247  + "@test.com")
+    end
+    context '255文字以上' do 
+      let(:email){ "a" * 247  + "@test.com" }
+      it 'is invalid' do 
         expect(user).to be_invalid
         expect(user.errors.messages[:email]).to include("は255文字以内で入力してください")
       end
-      describe 'メールフォーマットが不正' do
-        it '@の前に英数字、アンスコ(_)、記号(+,-,.)がない' do
-          user = build(:user, email: "@test.com")
-          expect(user).to be_invalid
-          expect(user.errors.messages[:email]).to include("は不正な値です")
-        end
-        it '@がない' do
-          user = build(:user, email: "testtest.com")
-          expect(user).to be_invalid
-          expect(user.errors.messages[:email]).to include("は不正な値です")
-        end
-        it '@の後に英数字、アンスコ(_)、記号(+,-,.)がない' do
-          user = build(:user, email: "test@.com")
-          expect(user).to be_invalid
-          expect(user.errors.messages[:email]).to include("は不正な値です")
-        end
-        it '@の後の文字列の後に.(ドット)がない' do
-          user = build(:user, email: "test@.com")
-          expect(user).to be_invalid
-          expect(user.errors.messages[:email]).to include("は不正な値です")
-        end
-        it '.(ドット)の後に数字がある' do
-          user = build(:user, email: "test@test.com1")
+    end
+    context 'メールフォーマットが不正' do
+      context '@の前に英数字、アンスコ(_)、記号(+,-,.)がない' do
+        let(:email){ "@test.com" }
+        it 'is invalid' do
           expect(user).to be_invalid
           expect(user.errors.messages[:email]).to include("は不正な値です")
         end
       end
-      it '一意ではない' do
-        user_a = create(:user)
-        user_b = build(:user)
-        expect(user_b).to be_invalid
-        expect(user_b.errors.messages[:email]).to include("はすでに存在します")
+      context '@がない' do
+        let(:email){ "testtest.com" }
+        it 'is invalid' do
+          expect(user).to be_invalid
+          expect(user.errors.messages[:email]).to include("は不正な値です")
+        end
+      end
+      context '@の後に英数字、アンスコ(_)、記号(+,-,.)がない' do
+        let(:email){ "test@.com" }
+        it 'is invalid' do
+          expect(user).to be_invalid
+          expect(user.errors.messages[:email]).to include("は不正な値です")
+        end
+      end
+      context '@の後の文字列の後に.(ドット)がない' do
+        let(:email){ "test@testcom" }
+        it 'is invalid' do
+          expect(user).to be_invalid
+          expect(user.errors.messages[:email]).to include("は不正な値です")
+        end
+      end
+      context '@の後の文字列の後に.(ドット)がない' do
+        let(:email){ "test@testcom" }
+        it 'is invalid' do
+          expect(user).to be_invalid
+          expect(user.errors.messages[:email]).to include("は不正な値です")
+        end
+      end
+      context '.(ドット)の後に数字がある' do
+        let(:email){ "test@test.com1" }
+        it 'is invalid' do
+          expect(user).to be_invalid
+          expect(user.errors.messages[:email]).to include("は不正な値です")
+        end
       end
     end
-    describe 'password' do
-      it '空白' do
-        user = build(:user, password: nil)
+    context '一意ではない' do
+      let(:email){ "test@test.com" }
+      it 'is invalid' do
+        create(:user)
+        expect(user).to be_invalid
+        expect(user.errors.messages[:email]).to include("はすでに存在します")
+      end
+    end
+  end
+  describe 'password' do
+    let(:user) { build(:user, password: password) }  
+    context '空白' do
+      let(:password){ nil }
+      it 'is invalid' do
         expect(user).to be_invalid
         expect(user.errors.messages[:password]).to include("を入力してください")
       end
-      it '6文字未満' do
-        user = build(:user, password: "aaaaa")
+    end
+    context '6文字未満' do
+      let(:password){ "aaaaa" }
+      it 'is invalid' do
         expect(user).to be_invalid
         expect(user.errors.messages[:password]).to include("は6文字以上で入力してください")
       end
-      it '暗号化' do
-        user = create(:user)
-        password = "password"
-        expect(user.password_digest == password ).to be_falsey
+    end
+    context '暗号化' do
+      let(:password){ "password" }
+      it 'is valid' do
+        expect(user).to be_valid
+        expect(user.password_digest == "password" ).to be_falsey
       end
     end
-    describe 'task_lists' do
-      it 'タスクリストを複数持つ' do
-        user = create(:user)
+  end
+  describe 'task_lists' do
+    let(:user) { build(:user) }
+    context 'タスクリストを複数持つ' do
+      it 'is valid' do
         create(:task_list, user: user)
         create(:task_list, user: user)
         expect(user.task_lists.count).to eq 2
       end  
-      it 'ユーザー削除でタスクリストも削除' do
-        user = create(:user)
-        task_list = create(:task_list, user: user)
+    end
+    context 'ユーザー削除でタスクリストも削除' do
+      it 'is valid' do
+        create(:task_list, user: user)
         expect{user.destroy}.to change{TaskList.count}.by(-1)
       end
-    end 
-  it '成功' do
-    user = create(:user)
-    expect(user).to be_valid
+    end
+  end
+  describe '成功' do
+    let(:user) { create(:user) }
+    it 'is vaid' do
+      expect(user).to be_valid
+    end
   end
 end
