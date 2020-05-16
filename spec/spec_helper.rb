35de9cc208374b44b955e25e77b0db84b6b14881
seiya2130 <satohseiya0312@gmail.com>
2020-05-16 18:05:58 +0900

    [fix]requestSpecのusersを修正

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,7 +23,7 @@ RSpec.configure do |config|
   
   config.before(:each, type: :system) do
     driven_by :selenium_chrome_headless
-  end
+  end 
 
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
